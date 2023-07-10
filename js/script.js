e6fbf0a1df85c610b8bc59a560706cd991c8165a
Manvel1996 <mdanielyan1996@gmail.com>
2023-07-10 19:00:07 +0400

    change unique cikl

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
 const API_KEY = "37bda1798910c78bef5284aed9201d60";
 const page = 5;
+
 let searchImgs = [];
 let basketNames = [];
 let basketsImages = [];
 let openBasket = "";
 let dragImg = {};
 
-const searchForm = document.querySelector(".imgs-form");
-const searchInput = document.querySelector(".imgs-form__search");
-const baskets = document.querySelector(".imgs-baskets");
-const imgsBoard = document.querySelector(".imgs-board--big");
-const basketBoard = document.querySelector(".imgs-board--small");
+const searchForm = document.querySelector(".images-form");
+const searchInput = document.querySelector(".images-form__search");
+const baskets = document.querySelector(".images-baskets");
+const imagesBoard = document.querySelector(".images-board--big");
+const basketBoard = document.querySelector(".images-board--small");
 
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal__close");
 const modalText = document.querySelector(".modal__text");
 
-function submit(e) {
-  e.preventDefault();
-
+function reset() {
   searchImgs = [];
   basketNames = [];
   basketsImages = [];
   openBasket = "";
   baskets.innerHTML = "";
-  imgsBoard.innerHTML = "";
+  imagesBoard.innerHTML = "";
   basketBoard.innerHTML = "";
+}
 
-  const imgsName = searchInput.value.trim().toLowerCase().split(" ");
+function submit(e) {
+  e.preventDefault();
 
-  const uniqueObj = {};
+  reset();
 
-  imgsName.forEach((el) => {
-    if (el !== "" && !uniqueObj[el]) {
-      uniqueObj[el] = null;
-    }
+  let imagesNames = searchInput.value.trim().split(" ");
+
+  imagesNames = imagesNames.filter((el, index, array) => {
+    return el.trim().length > 0 && array.indexOf(el.trim()) === index;
   });
 
-  Object.keys(uniqueObj).forEach((name) => {
+  imagesNames.forEach((name) => {
     new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
 
@@ -59,7 +60,7 @@ function submit(e) {
         if (data.photos.photo?.length > 0) {
           data.photos.photo.forEach((el) => {
             let obj = {
-              id: Date.now(),
+              // id: Date.now() + Math.random() * 15,
               title: name,
               img: el,
             };
@@ -68,8 +69,8 @@ function submit(e) {
           });
         }
 
-        imgsCreator(true);
-        basketNameCreator();
+        imagesCreator(true);
+        basketButtonCreator();
       })
       .catch(() => {
         openModal("Server error", "red");
@@ -77,8 +78,8 @@ function submit(e) {
   });
 }
 
-function imgsCreator(random) {
-  imgsBoard.innerHTML = "";
+function imagesCreator(random) {
+  imagesBoard.innerHTML = "";
 
   if (random) {
     searchImgs = searchImgs.sort(() => Math.random() - 0.5);
@@ -86,42 +87,52 @@ function imgsCreator(random) {
 
   searchImgs.forEach((el) => {
     const photo = document.createElement("img");
-    const imgSrc = `http://farm${el.img.farm}.staticflickr.com/${el.img.server}/${el.img.id}_${el.img.secret}.jpg`;
+    const imageSrc = `http://farm${el.img.farm}.staticflickr.com/${el.img.server}/${el.img.id}_${el.img.secret}.jpg`;
 
-    photo.classList.add("imgs-board__photo", "imgs-board__photo--draggable");
-    photo.setAttribute("src", imgSrc);
+    photo.classList.add(
+      "images-board__photo",
+      "images-board__photo--draggable"
+    );
+    photo.setAttribute("src", imageSrc);
     photo.setAttribute("alt", el.title);
     photo.setAttribute("data-id", el.img.id);
     photo.draggable = true;
 
-    imgsBoard.appendChild(photo);
+    imagesBoard.appendChild(photo);
   });
 }
 
-function basketNameCreator() {
+function basketButtonCreator() {
   baskets.innerHTML = "";
 
-  const uniqueObj = {};
+  let basketsNames = [];
 
   searchImgs.forEach((el) => {
-    if (el.img && !uniqueObj[el.title]) {
-      uniqueObj[el.title] = null;
+    if (el.img) {
+      basketsNames.push(el.title);
     }
   });
 
-  Object.keys(uniqueObj).forEach((el) => {
-    const basketName = document.createElement("div");
-    basketName.classList.add("imgs-baskets__block");
-    basketName.innerText = el;
-    baskets.appendChild(basketName);
+  basketsNames = basketsNames.reduce((acc, el) => {
+    if (acc.includes(el)) {
+      return acc;
+    }
+    return [...acc, el];
+  }, []);
+
+  basketsNames.forEach((el) => {
+    const basketButton = document.createElement("div");
+    basketButton.classList.add("images-baskets__block");
+    basketButton.innerText = el;
+    baskets.appendChild(basketButton);
   });
 }
 
-function basketImgsCreator(title) {
+function basketImagesCreator(title) {
   basketBoard.innerHTML = "";
 
   const groupName = document.createElement("p");
-  groupName.classList.add("imgs-board__title", "title");
+  groupName.classList.add("images-board__title", "title");
   groupName.innerText = title;
   basketBoard.appendChild(groupName);
 
@@ -129,7 +140,7 @@ function basketImgsCreator(title) {
     if (el.title === title) {
       el.imgs.forEach((img) => {
         const photo = document.createElement("img");
-        photo.classList.add("imgs-board__photo");
+        photo.classList.add("images-board__photo");
         photo.setAttribute("src", img);
         basketBoard.appendChild(photo);
       });
@@ -139,12 +150,12 @@ function basketImgsCreator(title) {
 
 function imgGoBasket() {
   searchImgs = searchImgs.filter((el) => el.img.id !== dragImg.id);
-  imgsCreator();
+  imagesCreator();
 }
 
 function dragStart(e) {
   if (e.target !== e.currentTarget) {
-    e.target.classList.add("imgs-board__photo--opacity");
+    e.target.classList.add("images-board__photo--opacity");
 
     dragImg["title"] = e.target.alt;
     dragImg["src"] = e.target.src;
@@ -154,8 +165,8 @@ function dragStart(e) {
 
 function dragLeave(e) {
   if (e.target !== e.currentTarget) {
-    e.target.classList.remove("imgs-baskets__block--green");
-    e.target.classList.remove("imgs-baskets__block--red");
+    e.target.classList.remove("images-baskets__block--green");
+    e.target.classList.remove("images-baskets__block--red");
   }
 }
 
@@ -163,20 +174,20 @@ function dragOver(e) {
   e.preventDefault();
   if (e.target !== e.currentTarget) {
     if (e.target.innerText === dragImg.title) {
-      e.target.classList.add("imgs-baskets__block--green");
-    } else e.target.classList.add("imgs-baskets__block--red");
+      e.target.classList.add("images-baskets__block--green");
+    } else e.target.classList.add("images-baskets__block--red");
   }
 }
 
 function dragend(e) {
-  e.target.classList.remove("imgs-board__photo--opacity");
+  e.target.classList.remove("images-board__photo--opacity");
 }
 
 function drop(e) {
   e.preventDefault();
   if (e.target !== e.currentTarget) {
-    e.target.classList.remove("imgs-baskets__block--red");
-    e.target.classList.remove("imgs-baskets__block--green");
+    e.target.classList.remove("images-baskets__block--red");
+    e.target.classList.remove("images-baskets__block--green");
 
     if (dragImg.title !== e.target.innerText) return (dragImg = {});
 
@@ -207,7 +218,7 @@ function drop(e) {
     dragImg = {};
 
     if (openBasket === e.target.innerText) {
-      basketImgsCreator(e.target.innerText);
+      basketImagesCreator(e.target.innerText);
     }
 
     if (!searchImgs.length) {
@@ -234,8 +245,8 @@ window.addEventListener("click", (event) => {
 
 searchForm.addEventListener("submit", submit);
 
-imgsBoard.addEventListener("dragstart", dragStart);
-imgsBoard.addEventListener("dragend", dragend);
+imagesBoard.addEventListener("dragstart", dragStart);
+imagesBoard.addEventListener("dragend", dragend);
 
 baskets.addEventListener("dragleave", dragLeave);
 baskets.addEventListener("dragover", dragOver);
@@ -250,6 +261,6 @@ baskets.addEventListener("click", (e) => {
       basketBoard.innerHTML = "";
       return;
     } else openBasket = e.target.innerText;
-    basketImgsCreator(e.target.innerText);
+    basketImagesCreator(e.target.innerText);
   }
 });
